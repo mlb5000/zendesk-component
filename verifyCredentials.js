6f3813b8c91a0d05fbff284ccc8c0905b4e0e520
Renat Zubairov <renat.zubairov@gmail.com>
2017-01-27 10:39:10 +0100

    Updated verifyCredentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,26 +1,19 @@
 'use strict';
-const co = require('co');
-const rp = require('request-promise');
+const Zendesk = require('zendesk-node-api');
 
-// This function will be called by the platform to verify credentials
-module.exports = function verifyCredentials(credentials, cb) {
+/**
+ * This function will be called by the platform to verify credentials
+ *
+ * @param credentials
+ * @returns {Promise}
+ */
+module.exports = function verifyCredentials(credentials) {
   console.log('Credentials passed for verification %j', credentials);
 
-  co(function*() {
-    console.log('Fetching user information');
-
-    const test = yield rp({
-      uri: 'https://cdn.elastic.io/test.json',
-      json: true
-    });
-
-    console.log('Fetched JSON value=%j', test);
-
-    console.log('Verification completed');
-
-    cb(null, {verified: true});
-  }).catch(err => {
-    console.log('Error occurred', err.stack || err);
-    cb(err , {verified: false});
+  const zendesk = new Zendesk({
+    url: `https://${credentials.subdomain}.zendesk.com`,
+    oauth: credentials.access_token
   });
+
+  return zendesk.tickets.list();
 };
